7c0716267f180428b6064d311edf2c5bdab0a68d
WesleyxBZ <wesleyxbzs@gmail.com>
2019-09-29 18:18:22 -0300

    Update em pacote de repositórios e alguns ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { WhoIAmComponent } from './who-i-am/who-i-am.component';
-import { AboutComponent } from './about/about.component';
-import { FooterComponent } from './footer/footer.component';
-import { EntryVisionComponent } from './entry-vision/entry-vision.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WhoIAmComponent } from './components/who-i-am/who-i-am.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EntryVisionComponent } from './components/entry-vision/entry-vision.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
